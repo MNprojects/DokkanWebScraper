65b31af556fc0384bbd9937454894d64f8405178
Michael Nash <michaelnash.work@gmail.com>
2022-07-01 16:58:50 +0100

    saving 1 character data locally

--- index.ts
+++ index.ts
@@ -4,13 +4,18 @@ import { readFile, writeFile } from "fs/promises";
 import { resolve } from "path";
 import { JSDOM } from 'jsdom';
 
-getData();
+getDokkanData();
 
-async function getData() {
-    const document = await fetchFromWebOrCache('https://dbz-dokkanbattle.fandom.com/wiki/Category:LR', false);
-    const links = extractLinks(document);
+async function getDokkanData() {
+    const document: Document = await fetchFromWebOrCache('https://dbz-dokkanbattle.fandom.com/wiki/Category:LR', false);
+    const links: string[] = extractLinks(document);
     saveData('LR-links', links)
-    const charactersData = extractCharactersData(links)
+
+    const characterDocument: Document = await fetchFromWebOrCache(links[0])
+    const characterData = extractCharacterData(characterDocument);
+    saveData('LR-characters', characterData)
+
+
 }
 
 
@@ -29,14 +34,14 @@ async function fetchFromWebOrCache(url: string, ignoreCache = false) {
     if (!existsSync(resolve(__dirname, '.cache'))) {
         mkdirSync('.cache')
     }
-    console.log(`Getting data for ${url}...`);
+    // console.log(`Getting data for ${url}...`);
 
     const fileName = resolve(__dirname, `.cache/${Buffer.from(url).toString('base64')}.html`)
 
     if (!ignoreCache &&
         existsSync(fileName,)
     ) {
-        console.log(`I read ${url} from cache`);
+        // console.log(`I read ${url} from cache`);
         const HTMLData = await readFile(
             fileName,
             { encoding: 'utf8' }
@@ -44,7 +49,7 @@ async function fetchFromWebOrCache(url: string, ignoreCache = false) {
         const dom = new JSDOM(HTMLData);
         return dom.window.document;
     } else {
-        console.log(`I fetched ${url} fresh`);
+        // console.log(`I fetched ${url} fresh`);
         const HTMLData = await fetchPage(url);
         if (!ignoreCache && HTMLData) {
             writeFile(
@@ -74,11 +79,53 @@ function saveData(fileName: string, data: unknown) {
         { encoding: 'utf8' })
 }
 
-function extractCharactersData(links: string[]) {
-    links.forEach(link => {
-        extractCharacterData(link)
-    });
+async function getCharactersData(links: string[]) {
+    const characterDocs = links.map(async link =>
+        await getCharacterData(link)
+    );
+    console.log(characterDocs);
+    return characterDocs
+
 }
-function extractCharacterData(link: string) {
-    fetchFromWebOrCache(link, false)
+async function getCharacterData(link: string) {
+    return await fetchFromWebOrCache(link, false);
 }
+function extractCharacterData(characterDocument: Document) {
+    const characterData = {
+        name: characterDocument.querySelector('.mw-parser-output > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > center:nth-child(1) > b')?.innerHTML.split('<br>')[0],
+        Title: characterDocument.querySelector('.mw-parser-output > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > center:nth-child(1) > b')?.innerHTML.split('<br>')[1],
+        MaxLevel: characterDocument.querySelector('.mw-parser-output > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > center:nth-child(1)')?.textContent,
+        MaxSALevel: characterDocument.querySelector('.mw-parser-output > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > center:nth-child(1)')?.innerHTML.split('>/')[1],
+        Rarity: characterDocument.querySelector('.size45px > span:nth-child(2) > a:nth-child(1)')?.getAttribute('title')?.split('Category:')[1],
+        Class: characterDocument.querySelector('.mw-parser-output > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(4) > center:nth-child(1) > a:nth-child(1)')?.getAttribute('title')?.split(' ')[0].split('Category:')[1],
+        Type: characterDocument.querySelector('.mw-parser-output > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(4) > center:nth-child(1) > a:nth-child(1)')?.getAttribute('title')?.split(' ')[1],
+        Cost: characterDocument.querySelector('.mw-parser-output > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(5) > center:nth-child(1)')?.textContent,
+        ID: characterDocument.querySelector('.mw-parser-output > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(6) > center:nth-child(1)')?.textContent,
+        ImageURL: characterDocument.querySelector('.size120px > img:nth-child(1)')?.getAttribute('src'),
+        LeaderSkill: characterDocument.querySelector('.righttablecard > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)')?.textContent,
+        SuperAttack: characterDocument.querySelector('.righttablecard > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1)')?.textContent,
+        UltraSuperAttack: characterDocument.querySelector('.righttablecard > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(1)')?.textContent,
+        Passive: characterDocument.querySelector('.righttablecard > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(8) > td:nth-child(1)')?.textContent,
+        Links: Array.from(characterDocument.querySelectorAll('.righttablecard > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(10) > td:nth-child(1) > center:nth-child(1) > span > a')).map(link => link.textContent),
+        Categories: Array.from(characterDocument.querySelectorAll('.righttablecard > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(12) > td:nth-child(1) > center:nth-child(1) > a')).map(link => link.textContent),
+        KiMeter: characterDocument.querySelector('.righttablecard > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > center:nth-child(1) > a:nth-child(1) > img:nth-child(1)')?.getAttribute('alt')?.split('.png')[0],
+        BaseHP: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > center:nth-child(1)')?.textContent,
+        MaxLevelHP: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(3) > center:nth-child(1)')?.textContent,
+        FreeDupeHP: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(4) > center:nth-child(1)')?.textContent,
+        RainbowHP: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(5) > center:nth-child(1)')?.textContent,
+        BaseAttack: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > center:nth-child(1)')?.textContent,
+        MaxLevelAttack: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(3) > center:nth-child(1)')?.textContent,
+        FreeDupeAttack: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(4) > center:nth-child(1)')?.textContent,
+        RainbowAttack: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(5) > center:nth-child(1)')?.textContent,
+        BaseDefence: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > center:nth-child(1)')?.textContent,
+        MaxDefence: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(3) > center:nth-child(1)')?.textContent,
+        FreeDupeDefence: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(4) > center:nth-child(1)')?.textContent,
+        RainbowDefence: characterDocument.querySelector('.righttablecard > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(5) > center:nth-child(1)')?.textContent,
+        KiMultiplier: characterDocument.querySelector('.righttablecard > table:nth-child(6) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)')?.textContent,
+
+    }
+    console.log(characterData);
+    return characterData
+
+}
+
